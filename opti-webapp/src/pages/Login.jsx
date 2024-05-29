56f7441a8b6950edae55812393393447583f3a33
nicoTC-04 <nicolastrevinoc@hotmail.com>
2024-05-29 09:25:19 -0600

    edited login

--- opti-webapp/src/pages/Login.jsx
+++ opti-webapp/src/pages/Login.jsx
@@ -7,8 +7,8 @@ import { useAuth } from './../components/AuthContext';
 import './../styles/pages/Login.css';
 
 function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('Usuario');
+  const [password, setPassword] = useState('Pass');
 
   const { isAuthenticated, isLoading, login } = useAuth();
   const navigate = useNavigate();
@@ -35,38 +35,38 @@ function Login() {
 
   return (
     <div className="login-wrapper">
-      <img src="/images/logoBTC.png" alt="Logo BTC" className="logo-btc" />
+      <img src="/images/Opti_Smart_Blanco.png" alt="Logo BTC" className="logo-btc" />
       <div className="login-background"></div>
-      {/* Bienvenido a BTC OPTI*/}
-      <div className="welcome-text">Bienvenido a BTC OPTI</div>
       <form onSubmit={handleSubmit} className="login-form">
-        <h2>INICIO DE SESIÓN</h2>
-        <div className="input-field">
-          <FiUser className="input-icon" />
-          <input
-            id="username"
-            type="text"
-            placeholder="Usuario"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div className="input-field">
-          <FiLock className="input-icon" />
-          <input
-            id="password"
-            type="password"
-            placeholder="Contraseña"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+        <h2 className='titulo'>INICIO DE SESIÓN</h2>
+        <div className='inputs'>
+          <div className="input-field">
+            <FiUser className="input-icon" />
+            <input
+              id="username"
+              type="text"
+              placeholder="Usuario"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-field">
+            <FiLock className="input-icon" />
+            <input
+              id="password"
+              type="password"
+              placeholder="Contraseña"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <a href="/recover" className="recover">Recuperar contraseña?</a>
         </div>
         <div className="checkbox-container">
           <input type="checkbox" id="remember-me" />
           <label htmlFor="remember-me">Recordarme</label>
         </div>
         <button type="submit" className="login-button">Login</button>
-        <a href="/recover" className="recover">Recuperar contraseña?</a>
       </form>
     </div>
   );
